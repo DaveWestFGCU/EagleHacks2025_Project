acd83aa9e2006d4f3197e7b874c7082b66c8cb27
David West <dawest6674@eagle.fgcu.edu>
2025-02-22 14:05:17 -0500

    Initial commit. Prompt to get ad copy.

---
+++ api/prompts/ad_text.py
@@ -0,0 +1,11 @@
+prompt_text = \
+"""Generate a headline, body text, and call to action for <keyword>. Each area must include \'<keyword>\' in its text.
+The title for this ad campaign is <title>.
+The concept for this ad campaign is <description>.
+The main takeaway that we want to impart is <key_message>.
+
+Return the response in the following format:
+<headline>Your Headline</headline>
+<body_text>your body text</body_text>
+<call_to_action>your call to action</call_to_action> 
+"""
