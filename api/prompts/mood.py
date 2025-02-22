5fca2cf0ff99c2a6dca7a17791250e1ebe46f250
DaveWestFGCU <130115905+DaveWestFGCU@users.noreply.github.com>
2025-02-21 23:31:24 -0500

    Initial commit. Prompt for determining a color for the ad.

---
+++ api/prompts/mood.py
@@ -0,0 +1,11 @@
+prompt = \
+"""Which of the following colors do you think would best represent <product>?
+Yellow (clarity, warmth, optimism)
+Orange (friendly, cheerful, enthusiasm)
+Red (energy, passion, appetite)
+Green (health, nature, wealth
+Purple (wisdom, royalty, creativity)
+Blue (Peace, Security, Reliability)
+
+Reply with only a single color from the following list [Yellow, Orange, Red, Green, Blue].
+"""
