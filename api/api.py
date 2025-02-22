a1e723fd6909d125be36ad41e7b0941ba0c856e0
DaveWestFGCU <130115905+DaveWestFGCU@users.noreply.github.com>
2025-02-22 15:28:21 -0500

    Refactored to put static directory (for completed advertisement images) within api directory.

--- api/api.py
+++ api/api.py
@@ -68,5 +68,9 @@ async def poll_job(job_id: str):
     )
 
 
-os.makedirs('static', exist_ok=True)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+try:
+    app.mount("/static", StaticFiles(directory="api/static"), name="static")
+
+except RuntimeError:
+    os.makedirs('./api/static', exist_ok=True)
+    app.mount("/static", StaticFiles(directory="api/static"), name="static")
