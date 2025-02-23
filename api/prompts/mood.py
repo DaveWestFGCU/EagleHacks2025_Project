8615e10519a239c23a264942b7dc9ecb841ec757
DaveWestFGCU <130115905+DaveWestFGCU@users.noreply.github.com>
2025-02-23 01:34:03 -0500

    Changed prompt to be more about emotions than color.

--- api/prompts/mood.py
+++ api/prompts/mood.py
@@ -1,11 +1,15 @@
 prompt = \
-"""Which of the following colors do you think would best represent <product>?
-Yellow (clarity, warmth, optimism)
-Orange (friendly, cheerful, enthusiasm)
-Red (energy, passion, appetite)
-Green (health, nature, wealth
-Purple (wisdom, royalty, creativity)
-Blue (Peace, Security, Reliability)
-
-Reply with only a single color from the following list [Yellow, Orange, Red, Green, Blue].
+"""Reply with three (3) categories whose associations represent <keyword> within two asterisks (i.e. **#**).
+<categories>
+0. excitement, passion, urgency, danger 
+1. warmth, energy, creativity 
+2. optimism, happiness, caution
+3. nature, growth, health
+4. trust, security, professionalism
+5. luxury, sophistication, creativity
+6. femininity, romance, sensitivity
+7. sophistication, power, mystery
+8. purity, simplicity, cleanliness
+</categories>
+ONLY reply with three categories by number from the previous list within asterisks (i.e. **#**).
 """
