b89e0476422dbff8647944d5bd12c14d65a30244
DaveWestFGCU <130115905+DaveWestFGCU@users.noreply.github.com>
2025-02-22 22:42:31 -0500

    Changed prompt to ask for images to have a yellow color scheme.

--- api/prompts/image_generation.py
+++ api/prompts/image_generation.py
@@ -2,6 +2,6 @@ prompt_text = \
 """Generate an image advertisement for <product> targeting <audience>. 
 The ad should feature \'<details>\'.
 Through creative visuals ensure the image conveys \'<emotion>\'. 
-Use lighting, composition, and color schemes that align with the tone of <product> to make the ad visually striking and memorable.
+Use lighting and composition to create a yellow color scheme that makes the ad visually striking and memorable.
 Focus on aspects of this concept that do NOT include text, numbers, logos or typography.
 """
