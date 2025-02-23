47b443d22d0dc10a26e20bad03afda525a638cb9
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-02-23 01:35:28 -0500

    Begin documenting the code

--- app/db_init.py
+++ app/db_init.py
@@ -1,3 +1,20 @@
+"""Initialize the database.
+
+This module is intended only for standalone usage so that you can create
+and populate the database before running the backend.
+
+To refresh the database following an update to 'db_init.py', drop the
+database's contents using 'db_drop.py' before re-running 'db_init.py'.
+
+You can only create a database by connecting to an existing
+database, and similarly, you can only create a user by connecting
+as an existing user. To work around that, we assume that we can
+connect to the 'postgres' database as the 'postgres' user. You
+must supply the password via the 'PGPASSWORD' environment variable.
+The 'PGPASSWORD' environment variable and more are documented in
+<https://www.postgresql.org/docs/current/libpq-envars.html>.
+"""
+
 import contextlib
 
 from psycopg.errors import DuplicateDatabase, DuplicateObject
@@ -7,9 +24,11 @@ import db
 
 
 def main():
+    # Don't worry if the user already exists.
     with contextlib.suppress(DuplicateObject):
         _create_user()
 
+    # Similarly, don't worry if the database already exists.
     with contextlib.suppress(DuplicateDatabase):
         _create_db()
 
